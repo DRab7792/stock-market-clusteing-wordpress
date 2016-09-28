a830bc1af73a9173daccd48242f8af2a0d52ca61
Dan Rabinowitz <danr@sanbornmediafactory.com>
2016-09-28 00:26:37 -0400

    Removing cpt and adding options page

--- inc/post_types.php
+++ inc/post_types.php
@@ -2,31 +2,4 @@
 
 add_action( 'init', 'cptui_register_my_cpts' );
 function cptui_register_my_cpts() {
-	$labels = array(
-		"name" => __( 'Interactives', 'portfilo' ),
-		"singular_name" => __( 'Interactive', 'portfilo' ),
-		);
-
-	$args = array(
-		"label" => __( 'Interactives', 'portfilo' ),
-		"labels" => $labels,
-		"description" => "",
-		"public" => true,
-		"show_ui" => true,
-		"show_in_rest" => false,
-		"rest_base" => "",
-		"has_archive" => false,
-		"show_in_menu" => true,
-		"exclude_from_search" => false,
-		"capability_type" => "post",
-		"map_meta_cap" => true,
-		"hierarchical" => false,
-		"rewrite" => array( "slug" => "world", "with_front" => true ),
-		"query_var" => true,
-				
-		"supports" => array( "title" ),				
-	);
-	register_post_type( "world", $args );
-
-// End of cptui_register_my_cpts()
 }
